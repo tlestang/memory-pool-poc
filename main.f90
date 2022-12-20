336356e93479f481433160a6e81611a788dbd3c6
Thibault Lestang <t.lestang@imperial.ac.uk>
2022-12-20 10:46:20 +0000

    Update main program to test assignment of memory_block_t objs

--- main.f90
+++ main.f90
@@ -1,12 +1,25 @@
 program main
   use field
+  use pool_module
 
   type(field_t) :: u, v
-  u = field_t()
-  call random_number(u%data)
-  v = u
 
-  write(*, *) sum(u%data)
-  write(*, *) sum(v%data)
+  call init_memory_pool(3, 16)
+  call print_freelist()
+  write(*,*) 'Init u field'
+  u = field_t()
+  write(*,*) 'U: ', u%data%id, u%data%refcount
+  call print_freelist()
+  write(*,*) 'Init v field'
+  v = field_t()
+  write(*,*) 'V: ', v%data%id, v%data%refcount
+  call print_freelist()
+  write(*,*) 'Assiginig v field'
+  v = u
+  call print_freelist()
+  write(*,*) 'v%data%id = ', v%data%id
+  write(*,*) 'u%data%id = ', u%data%id
+
+  call finalise_memory_pool()
 
 end program main
