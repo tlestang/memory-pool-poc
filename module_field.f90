1f1720440cd56673122ed65709df7f7dd0ea7f27
Thibault Lestang <t.lestang@imperial.ac.uk>
2022-12-20 10:43:09 +0000

    Initialise memory_block_t pointer to null in field type

--- module_field.f90
+++ module_field.f90
@@ -4,7 +4,7 @@ module field
   implicit none
 
   type field_t
-     type(memory_block_t), pointer :: data
+     type(memory_block_t), pointer :: data => null()
    contains
      final :: field_destructor
   end type field_t
@@ -25,13 +25,14 @@ contains
 
   subroutine field_destructor(self)
     type(field_t), intent(inout) :: self
-    call unbind_or_release(self%data)
+    if(associated(self%data)) then
+       call unbind_or_release(self%data)
+    end if
   end subroutine field_destructor
 
   subroutine field_from_field(a, b)
     type(field_t), intent(out) :: a
     type(field_t), intent(in) :: b
-    call unbind_or_release(a%data)
     a%data => b%data
     b%data%refcount = b%data%refcount + 1
   end subroutine field_from_field
